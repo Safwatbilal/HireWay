54e79dddc8170eadd788dfca53e9cd677d5e9082
Safwatbilal <91734786+Safwatbilal@users.noreply.github.com>
2025-01-22 21:27:07 +0300

    Delete house/room directory

--- house/room/components/AuthWrapper.jsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import { AuthProvider } from "@/store/aviblity";
-const AuthWrapper = ({ children }) => {
-  return <AuthProvider>{children}</AuthProvider>;
-};
-
-export default AuthWrapper;
